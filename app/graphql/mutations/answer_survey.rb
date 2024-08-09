90489a7268507a9e7df0f864d9e69fed707bf087
Pjmaciel <pjmaciel_4@hotmail.com>
2024-08-08 22:51:19 -0300

    feat(answer): implement answerSurvey mutation with validations for mandatory questions and response types

---
+++ app/graphql/mutations/answer_survey.rb
@@ -0,0 +1,54 @@
+module Mutations
+  class AnswerSurvey < Mutations::BaseMutation
+    argument :survey_id, ID, required: true
+    argument :responses, [Types::Input::ResponseInput], required: true
+
+    type Types::Survey
+
+    def resolve(survey_id:, responses:)
+      survey = Survey.find(survey_id)
+
+
+      answers = {}
+
+      responses.each do |response_input|
+        user = User.find(response_input.user_id)
+        raise GraphQL::ExecutionError, "Invalid user" unless user
+
+        question = survey.questions.find(response_input.question_id)
+
+
+        validate_response(question, response_input.response_text)
+
+
+        answers[question.id] = Response.create!(
+          question: question,
+          user: user,
+          response_text: response_input.response_text
+        )
+      end
+
+      survey.questions.each do |question|
+        if question.required? && !answers.key?(question.id)
+          raise GraphQL::ExecutionError, "The question #{question.id} is mandatory and has not been answered."
+        end
+      end
+
+      survey
+    end
+
+    private
+
+    def validate_response(question, response_text)
+      case question.question_type
+      when 'radio', 'checkbox'
+        valid_option = question.options.pluck(:option_text).include?(response_text)
+        raise GraphQL::ExecutionError, "Invalid answer to the question #{question.id}" unless valid_option
+      when 'text'
+        raise GraphQL::ExecutionError, "Text response cannot be blank" if response_text.blank?
+      else
+        raise GraphQL::ExecutionError, "Unknown question type"
+      end
+    end
+  end
+end
