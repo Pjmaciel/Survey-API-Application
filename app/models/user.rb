e11220c8e369b2022bacc6f97d07458fb1689ac1
Pjmaciel <pjmaciel_4@hotmail.com>
2024-08-09 11:26:02 -0300

    feat(Devise): Implement authentication system and Create middleware to authorize research coordinators.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum role: { user: 'user', coordinator: 'coordinator', admin: 'admin' }
+
+  def coordinator?
+    role == 'coordinator'
+  end
 end
