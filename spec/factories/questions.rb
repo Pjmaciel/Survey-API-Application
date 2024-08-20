4d68bebad06d1c1b83e01250248bc8b9a6f19606
Pjmaciel <pjmaciel_4@hotmail.com>
2024-08-20 19:24:14 -0300

    fix: update factories to reflect new model validations

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,7 +1,12 @@
 FactoryBot.define do
   factory :question do
-    survey { nil }
-    question_text { "MyString" }
-    question_type { "MyString" }
+    association :survey
+    question_text { 'Qual é a sua cor favorita?' }
+    question_type { 'multiple_choice' }
+
+    # Cria 3 opções por padrão após criar a pergunta
+    after(:create) do |question|
+      create_list(:option, 3, question: question) # Ajuste o número aqui se necessário
+    end
   end
 end
