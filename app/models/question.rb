e5ea01b390c800fcba80475aaef4b5c8e9a2c962
Pjmaciel <pjmaciel_4@hotmail.com>
2024-08-20 19:23:14 -0300

    fix: update models with new validations and relationships

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,17 @@
 class Question < ApplicationRecord
   belongs_to :survey
   has_many :options, dependent: :destroy
-  has_many :responses
+  has_many :responses, dependent: :destroy
+
+  validate :validate_options_count
+
+  accepts_nested_attributes_for :options, allow_destroy: true
+
+  private
+
+  def validate_options_count
+      if options.size < 1 || options.size > 5
+        errors.add(:options, 'must have between 1 and 5 options')
+      end
+    end
 end
