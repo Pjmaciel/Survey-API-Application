17069624813a353debfb39cfb69855cf66ca8ec0
Pjmaciel <pjmaciel_4@hotmail.com>
2024-08-20 19:22:40 -0300

    fix: update GraphQL types for new requirements

--- app/graphql/types/question.rb
+++ app/graphql/types/question.rb
@@ -3,13 +3,6 @@ module Types
     field :id, ID, null: false
     field :question_text, String, null: false
     field :question_type, String, null: false
-    field :survey, Types::Survey, null: false
     field :options, [Types::Option], null: true
-    field :responses, [Types::Response], null: true
-    field :required, Boolean, null: false
-
-    def responses
-      object.responses
-    end
   end
 end
--- app/graphql/types/survey.rb
+++ app/graphql/types/survey.rb
@@ -2,7 +2,15 @@ module Types
   class Survey < Types::BaseObject
     field :id, ID, null: false
     field :title, String, null: false
-    field :user, Types::User, null: false
+    field :status, String, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
     field :questions, [Types::Question], null: true
+    field :user, Types::User, null: false
+
+    def questions
+      object.questions.includes(:responses)
+    end
+
   end
 end
